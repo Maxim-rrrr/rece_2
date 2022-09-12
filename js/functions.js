ac352b520b67ea417fb7503e9ac74b09f0f9a352
Maxim-rrrr <kewin.rrrr@gmail.com>
2022-09-12 12:16:17 +0300

    +: func crossing line detect

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+function VEK(ax,ay,bx,by)//векторное произведение
+{
+	return ax*by-bx*ay;
+}
+
+function cross(p1,p2,p3,p4) //проверка пересечения
+{
+	var v1,v2,v3,v4;
+ 
+	v1=VEK(p4[0] - p3[0], p4[1] - p3[1], p1[0] - p3[0], p1[1] - p3[1]);
+	v2=VEK(p4[0] - p3[0], p4[1] - p3[1], p2[0] - p3[0], p2[1] - p3[1]);
+	v3=VEK(p2[0] - p1[0], p2[1] - p1[1], p3[0] - p1[0], p3[1] - p1[1]);
+	v4=VEK(p2[0] - p1[0], p2[1] - p1[1], p4[0] - p1[0], p4[1] - p1[1]);
+	if(v1*v2<0 && v3*v4<0) return true;
+	else return false;
+}
